42c7d6d36deb97f765170fdc3e78dec6a4482209
unknown <paolocassina99@gmail.com>
2021-03-04 14:52:33 +0100

    Improved backend.js

--- backend.js
+++ backend.js
@@ -1,7 +1,12 @@
 function purchase() {
+     window.location.href = "./order.html";
 	
 }
 
 function order() {
+     city
+document.getElementbyId("city").value;
+
+      alert("We will ship to" + city + "!");
 
 }
